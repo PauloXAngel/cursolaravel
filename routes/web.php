4649f3e5398b0581f2a87a955dc533cba749b8db
pauloxangel <185548830+PauloXAngel@users.noreply.github.com>
2025-12-03 15:01:34 -0300

    Criação de novos controllers
    aprendi um pouco mais sobre a estrutura MVC , criação de novos controllers, e atribuições de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::any('/any', function(){
-    return "Permite todo tipo de acesso http (GET, POST, PUT, DELETE...)";
-});
-
-Route::match(['get','post'], '/match', function(){
-    return "Permite apenas os tipos de acesso http definidos no array";
-
-});
-
-Route::get('/produto/{id}/{cat?}', function($id, $cat = ""){
-    return "O id do produto é: ".$id."<br> A categoria é: ".$cat;
-});
-
-
-// o laragon esta no drive para efeturar o download
+Route::resource('produtos', ProdutoController::class);
